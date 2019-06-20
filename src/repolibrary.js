d2d0ec2c0e59851b720b091011ef9c0662c04ece
Mace <maceplus@gmail.com>
2019-06-20 02:08:20 +0100

    remove data variable & getReposFrom fuction

--- src/repolibrary.js
+++ src/repolibrary.js
@@ -1,29 +1,16 @@
 async function getReposFor(gitUser) {
   const BASE_URL = "https://api.github.com/users/"
-  const response = await fetch(`${BASE_URL} ${gitUser}/repos`);
+  const response = await fetch(`${BASE_URL}${gitUser}/repos`);
   if (!response.ok) {
     console.error('Error fetching from API');
   }
   try {
-    const data = await response.json();
-    const repo = data.data;
-    return repo;
-
-  } catch (e) {
+   return await response.json();
+} catch (e) {
     console.error(e);
   }
 }
 
-function getReopsFrom(repo, gitUser, repoList) {
-  const gits = repoList
-  return gits.map((gitUser) => ({
-    gitUser,
-    value: repo[gitUser]
-  }));
-}
-
-
-export async function getRepos(gitUser, repoList = []) {
-  const repo = await getReposFor(gitUser);
-  return getReopsFrom(repo, gitUser, repoList);
+export async function getRepos(gitUser) {
+  return await getReposFor(gitUser);
 }
